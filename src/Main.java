93d4a2f7b64f4068813fc04eedf05612e38d415f
Andrei Zola <anzola@yandex.ru>
2021-11-23 17:17:23 +0100

    Stream

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(new OneZeroStream());
-        for (int i = 0; i < 10; i++) {
-            System.out.print(scanner.nextInt());
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        FileOutputStream fos = new FileOutputStream("text.txt");
+        fos.write(string.getBytes(StandardCharsets.UTF_8));
+        fos.close();
+        reader.close();
     }
 }
